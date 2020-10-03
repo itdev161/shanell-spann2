77346f6d9601453fd89356f0728be100eea5d11d
Your Nam <you@example.com>
2020-10-02 22:47:21 -0500

    Completed Activity 4

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,24 @@
-import React from 'react';;
+import React from 'react';
 import './App.css';
+import axios from 'axios';
 
 class App extends React.Component {
+  state = {
+    data: null
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:5000')
+    .then((response) => {
+      this.setState({
+        data: response.data
+      })
+    })
+    .catch((error) => {
+      console.error(`Error fetching data: ${error}`);
+    })
+  }
+  
   render() {
     return (
       <div className="App">
