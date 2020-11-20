3d84d925ee1f641a4dd6bc66b2b4baa51f64807e
Your Nam <you@example.com>
2020-11-20 15:56:06 -0600

    Created an Endpoint to Get Posts

--- server.js
+++ server.js
@@ -208,3 +208,18 @@ app.post(
         }
     }
 );
+
+/**
+ * @route GET api/posts
+ * @desc Get posts
+ */
+app.get('/api/posts', auth, async(req, res) => {
+    try {
+        const posts = await Post.find().sort({ date: -1 });
+
+        res.json(posts);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Server error');
+    }
+});
